c59f3772f20079958a4f2555019fbb4dda68c8fe
Paragonzm <zuneidkaemba@gmail.com>
2025-05-08 12:01:24 +0530

    Update PhishMailer.py

--- PhishMailer.py
+++ PhishMailer.py
@@ -11,43 +11,49 @@ from Core.eletter import Gmail
 from Core.eletter import Spotify
 from Core.devicemenu import Linkedin
 
+# Color codes
+red = "\033[1;31;40m"
+green = "\033[1;32;40m"
+white = "\033[1;37;40m"
+blue = "\033[1;34;40m"
 
-red = ("\033[1;31;40m")
-green = ("\033[1;32;40m")
-white = ("\033[1;37;40m")
-blue = ("\033[1;34;40m")
-
-os.system("clear")
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
 
 def CurrentDir():
-	path = os.getcwd()
-	print(green + "[" + white + "+" + green + "]" + white + " Your Templates Will Be Saved Here " + path + '/"TemplateName.html"')
+    path = os.getcwd()
+    print(green + "[" + white + "+" + green + "]" + white +
+          " Your Templates Will Be Saved Here: " + path + '/"TemplateName.html"')
 
+def get_user_choice():
+    print(green + "[1] Instagram\n[2] Facebook\n[3] Gmail\n[4] Linkedin\n[5] Spotify")
+    try:
+        return int(input(green + "root@phishmailer:~ " + white))
+    except ValueError:
+        print(red + "Please enter a valid number.")
+        return None
+
+def run_selected_module(choice):
+    modules = {
+        1: Instagram,
+        2: Facebook,
+        3: Gmail,
+        4: Linkedin,
+        5: Spotify
+    }
+    action = modules.get(choice)
+    if action:
+        action()
+    else:
+        print(red + "Invalid option selected.")
 
 def mainMenu():
-	menu()
+    clear_screen()
+    CurrentDir()
+    choice = get_user_choice()
+    if choice:
+        run_selected_module(choice)
 
-	print(green)
-	
-	CurrentDir()
-	
-	mailPick = int(input(green + "root@phishmailer:~ " + white))
+if __name__ == "__main__":
+    mainMenu()
 
-	try:
-
-            if mailPick == 1:
-                    Instagram()
-
-            elif mailPick == 2:
-                    Facebook()
-
-            elif mailPick == 3:
-                    Gmail()
-
-            elif mailPick == 4:
-                    Linkedin()
-
-            elif mailPick == 5:
-                    Spotify()
-
-mainMenu()
